60f47c24aaaa87df6c73f0400ca4a9c2b8f48ccf
Manoj2204 <mahtomanojkumar824@gmail.com>
2023-11-30 10:31:56 +0530

    commited

---
+++ src/test/java/Parameter/Parameter_Test.java
@@ -0,0 +1,44 @@
+package Parameter;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Parameter_Test 
+{
+	public WebDriver driver;
+	@Parameters({"browser","url"})
+	@BeforeMethod
+	public void config_BeforeMethod(String browser, String url)
+	{
+		if (browser.equalsIgnoreCase("chrome")) 
+		{
+			driver= new ChromeDriver();
+		}
+		else if (browser.equalsIgnoreCase("edge")) 
+		{
+			driver = new EdgeDriver();	
+		}
+		driver.get(url);
+		driver.manage().window().maximize();
+	}
+	
+	@AfterMethod
+	public void config_AfterMethod()
+	{
+		driver.manage().window().minimize();
+		driver.close();
+	}
+	
+	@Test
+	public void test()
+	{
+		String title = driver.getTitle();
+		System.out.println(title);
+	}
+
+}
